6935a814e1da74a4cba272cf7c891c430c506eb3
dmshvetsov <shvetsovdm@gmail.com>
2026-01-08 19:28:55 +0700

    make CS policies more stict

--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,9 @@ export interface CSPPluginOptions {
 
 const defaultDirectives: CSPDirectives = {
   "default-src": "'self'",
-  "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-  "style-src": ["'self'", "'unsafe-inline'"],
-  "img-src": ["'self'", "data:", "https:"],
+  "script-src": ["'self'"],
+  "style-src": ["'self'"],
+  "img-src": ["'self'", "data:"],
   "font-src": "'self'",
   "connect-src": "'self'",
   "media-src": "'self'",
